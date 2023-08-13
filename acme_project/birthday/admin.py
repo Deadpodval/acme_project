37bc1cc9b27fa43f4ee53336fe03a8a3d234894c
Deadpodval <timofei_kahharov@mail.ru>
2023-08-13 17:50:11 +0600

    CBV-step

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+
+
+from .models import Birthday
+
+
+@admin.register(Birthday)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "birthday"]
+    search_fields = ["first_name", "last_name", "birthday"]
+    list_filter = ["first_name", "last_name", "birthday"]
